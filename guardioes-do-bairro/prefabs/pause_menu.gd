7cd5866d9185b631b5c7a45c5da6203ad3cba043
oman ramalho <omanapple42@hotmail.com>
2026-05-12 19:09:30 -0300

    feat/create pause screen

---
+++ guardioes-do-bairro/prefabs/pause_menu.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+@onready var resume_btn = $menu_holder/pause_btn
+
+func _ready() -> void:
+	visible = false
+	
+func _process(delta) -> void:
+	pass
+
+func _on_pause_btn_pressed() -> void:
+	get_tree().paused = false
+	visible = true
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		visible = true
+		get_tree().paused = true
+		resume_btn.grab_focus()
+
+func _on_quit_btn_pressed() -> void:
+	get_tree().quit()
